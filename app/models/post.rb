b9c26e39aa5f410aa32ba92da14e113b12d45e5a
GAP <gapnav@gmail.com>
2016-11-05 14:04:47 +0200

    protect from repeating

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,15 @@ class Post < ApplicationRecord
 
   validates :body, presence: true
   validates_length_of :body, maximum: 255
+  validate :validate_repeating
+
+  private
+
+  def validate_repeating
+    prev_post = Post.where(user: self.user).last
+    if prev_post && prev_post.body == self.body
+      self.errors[:base] << 'This post repeats your previous one'
+    end
+  end
+
 end
