f3a65e176525dd0dad5c51f22f163697547c1d78
GAP <gapnav@gmail.com>
2016-11-05 13:56:22 +0200

    post create feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html
+
+  self.responder = ApplicationResponder
+  respond_to :html
+  responders :flash
   protect_from_forgery with: :exception
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,26 @@
 class PostsController < ApplicationController
+  before_action :new_post, only: [:index, :new]
+
   def index
     @posts = Post.includes(:user).order(:created_at)
+  end
+
+  def new; end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.save
+    respond_with @post, location: posts_path, notice: 'New entry was added successfully'
+  end
+
+  private
+
+  def new_post
+    @post = Post.new
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
